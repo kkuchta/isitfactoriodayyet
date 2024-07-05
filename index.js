6e79864c91f2e2b6fd10117d4d94a3f97364b379
Kevin Kuchta <kevin@kevinkuchta.com>
2024-07-05 12:17:28 -0700

    Initial revision

---
+++ index.js
@@ -0,0 +1,38 @@
+const update = () => {
+  function getTimeRemaining(duration) {
+    let str = "";
+    const days = Math.floor(duration / (1000 * 60 * 60 * 24));
+    if (days > 0) {
+      str += `${days} days, `;
+    }
+    const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    if (days > 0 && hours > 0) {
+      str += `${hours} hours, `;
+    }
+    const minutes = Math.floor((duration / 1000 / 60) % 60);
+    if (days > 0 && hours > 0 && minutes > 0) {
+      str += `${minutes} minutes, and `;
+    }
+    const seconds = Math.floor((duration / 1000) % 60);
+    if (seconds > 0) {
+      str += `${seconds} seconds`;
+    }
+    return str;
+  }
+
+  const releaseDate = new Date("2024-10-21 00:00:00");
+  const durationRemaining = releaseDate - Date.parse(new Date());
+  const remainingString = getTimeRemaining(durationRemaining);
+
+  const answerDiv = document.getElementById("answer");
+  const remainingDiv = document.getElementById("remaining");
+  if (durationRemaining > 0) {
+    answerDiv.textContent = "No.";
+    remainingDiv.textContent = remainingString + " remaining."
+  } else {
+      answerDiv.textContent = "Yes!";
+      remainingDiv.textContent = "The Factory Must Grow";
+  }
+}
+update();
+setInterval(update, 1000);
