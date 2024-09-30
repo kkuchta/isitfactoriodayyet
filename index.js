386283851a0be2922ab52146acc3d5119594817c
vivaloz <olafjanse10@gmail.com>
2024-09-30 18:09:46 +0200

    added progress bar
    thought it would be neat.

--- index.js
+++ index.js
@@ -20,9 +20,20 @@ const update = () => {
     return str;
   }
 
-  const releaseDate = new Date("2024-10-21 00:00:00");
-  const durationRemaining = releaseDate - Date.parse(new Date());
-  const remainingString = getTimeRemaining(durationRemaining);
+  function calcProgress(releaseDate, currentDate, announcement, dec){ // calculate progress as a percentage
+    let pr = (currentDate - announcement) / (releaseDate - announcement) * 100; // calculate percentage of time elapsed from announcement - release
+    if(pr < 0) return 0; // date before announcement
+    if(pr > 100) return 100; // data after release
+    return Math.round(pr * (10**dec)) / (10**dec); // round to $dec decimals
+  }
+
+  const announcement = Number(new Date("2024-07-05 00:00:00")); // announcement date according to fff 418
+  const releaseDate = Number(new Date("2024-10-21 00:00:00"));
+  const currentDate = Date.parse(new Date());
+  const durationRemaining = releaseDate - currentDate;
+  const remainingString = getTimeRemaining(durationRemaining); 
+  const decimals = 6; // amount of decimals to round progress to
+  const progress = calcProgress(releaseDate, currentDate, announcement, decimals); // I dislike this way of handling functions and variables but consistency is more important
 
   const answerDiv = document.getElementById("answer");
   const remainingDiv = document.getElementById("remaining");
@@ -33,6 +44,14 @@ const update = () => {
       answerDiv.textContent = "Yes!";
       remainingDiv.textContent = "The Factory Must Grow";
   }
+
+  const progressEl = document.getElementById("progress"); // oh how i love unefficient code like this. But tis your project, after all.
+  progressEl.value = progress;
+  const percentEl = document.getElementById("percentage"); // oh how i love unefficient code like this. But tis your project, after all.
+  const days = Math.floor(durationRemaining / (1000 * 60 * 60 * 24));
+  const totaldays = Math.floor((releaseDate - announcement) / (1000 * 60 * 60 * 24));
+  percentEl.innerText = "Progress: " + progress + "% - Day " + (totaldays - days) + " of " + totaldays;
+
 }
 update();
 setInterval(update, 1000);
